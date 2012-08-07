b685aa916ad9d5867418411e70db18a480707bdb
Bharata Kalbuaji <18209018@std.stei.itb.ac.id>
2012-08-07 15:24:45 +0700

    Beresin konflik
    Signed-off-by: Bharata Kalbuaji <18209018@std.stei.itb.ac.id>

--- application/controllers/perencanaan/dashboard.php
+++ application/controllers/perencanaan/dashboard.php
@@ -17,14 +17,6 @@ class Dashboard extends Perencanaan_Controller{
         $this->thn_default = date('Y');
         $this->load->model('mdl_perencanaan','rnc');
     }
-
-<<<<<<< HEAD
-    protected $thn_default;
-    public function __construct() {
-        parent::__construct();
-        $this->thn_default = date('Y');
-        $this->load->model('mdl_perencanaan','rnc');
-    }
     
     public function index(){
         $this->home();
@@ -51,13 +43,5 @@ class Dashboard extends Perencanaan_Controller{
             $data['pil_kategori'][$k['id']]=$k['name'];
         }
         $this->template->display('main/perencanaan/form_buat_diklat',$data);
-=======
-    public function index(){
-        //$this->load->library('lib_perencanaan');
-	$data['title']='Bidang Perencanaan';
-        $data['kategori']=$this->rnc->get_kategori();
-        $data['program']=$this->rnc->get_program($this->thn_default);
-	$this->template->display('main/perencanaan/index',$data);
->>>>>>> 3576fb2369a120f75e01d1e95ef205c5ebcd4d27
     }
 }
