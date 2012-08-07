136983ddd9901ff86683021a787d0b4865e23854
Bharata Kalbuaji <18209018@std.stei.itb.ac.id>
2012-08-07 16:09:07 +0700

    Edit ama delete program
    Signed-off-by: Bharata Kalbuaji <18209018@std.stei.itb.ac.id>

--- application/controllers/perencanaan/dashboard.php
+++ application/controllers/perencanaan/dashboard.php
@@ -78,4 +78,32 @@ class Dashboard extends Perencanaan_Controller{
         }
         $this->template->display('main/perencanaan/form_edit_diklat',$data);
     }
+    
+    function update_diklat(){
+        $clause=$this->input->post('id');
+        
+        $data['name']=$this->input->post('name');
+        $data['parent']=$this->input->post('kategori');
+        $data['tanggal_mulai']=$this->input->post('tanggal_mulai');
+        $data['tanggal_akhir']=$this->input->post('tanggal_akhir');
+        $data['deskripsi']=$this->input->post('deskripsi');
+        $data['tujuan']=$this->input->post('tujuan');
+        $data['indikator']=$this->input->post('indikator');
+        $data['pelaksanaan']=$this->input->post('pelaksanaan');
+        $data['lama_pendidikan']=$this->input->post('lama_pendidikan');
+        $data['deskripsi']=$this->input->post('deskripsi');
+        $data['persyaratan']=$this->input->post('persyaratan');
+        $data['materi']=$this->input->post('materi');
+        $data['pelaksana']=$this->input->post('pelaksana');
+        $data['fasilitator']=$this->input->post('fasilitator');
+        $data['jumlah_peserta']=$this->input->post('jumlah_peserta');
+        $data['tempat']=$this->input->post('tempat');
+        $data['tahun_program']=$this->input->post('tahun_program');
+        
+        $this->rnc->update_diklat($clause,$data);
+    }
+    
+    function delete_diklat($id){
+        $this->rnc->delete_diklat($id);
+    }
 }
--- application/views/main/perencanaan/form_edit_diklat.php
+++ application/views/main/perencanaan/form_edit_diklat.php
@@ -1,20 +1,21 @@
 Form Pembuatan Diklat Baru
-<form method="post" action="perencanaan/dashboard/insert_diklat">
+<form method="post" action="perencanaan/dashboard/update_diklat">
+    <input type="hidden" name="id" value="<?php echo $program['id'] ?>"/>
     <div>Nama : <input type="text" name="name" value="<?php echo $program['name'] ?>"/></div>
     <div>Kategori : <?php echo form_dropdown('kategori', $pil_kategori, $program['parent']) ?></div>
     <div>Tanggal mulai : <input type="date" name="tanggal_mulai" value="<?php echo $program['tanggal_mulai'] ?>"/></div>
     <div>Tanggal selesai : <input type="date" name="tanggal_akhir" value="<?php echo $program['tanggal_akhir'] ?>"/></div>
     <div>Deskripsi : <?php echo $this->editor->textarea('deskripsi',$program['deskripsi']) ?></div>
-    <div>Tujuan : <?php echo $this->editor->textarea('tujuan') ?></div>
-    <div>Indikator : <?php echo $this->editor->textarea('indikator') ?></div>
-    <div>Pelaksanaan : <?php echo $this->editor->textarea('pelaksanaan') ?></div>
-    <div>Lama pendidikan : <?php echo $this->editor->textarea('lama_pendidikan') ?></div>
-    <div>Persyaratan : <?php echo $this->editor->textarea('persyaratan') ?></div>
-    <div>Materi : <?php echo $this->editor->textarea('materi') ?></div>
-    <div>Pelaksana : <?php echo $this->editor->textarea('pelaksana') ?></div>
-    <div>Fasilitator : <?php echo $this->editor->textarea('fasilitator') ?></div>
-    <div>Jumlah peserta : <input type="text" name="jumlah_peserta"/></div>
-    <div>Tempat : <?php echo $this->editor->textarea('tempat') ?></div>
-    <div>Tahun program : <input type="text" name="tahun_program"/></div>
+    <div>Tujuan : <?php echo $this->editor->textarea('tujuan',$program['tujuan']) ?></div>
+    <div>Indikator : <?php echo $this->editor->textarea('indikator',$program['indikator']) ?></div>
+    <div>Pelaksanaan : <?php echo $this->editor->textarea('pelaksanaan',$program['pelaksanaan']) ?></div>
+    <div>Lama pendidikan : <?php echo $this->editor->textarea('lama_pendidikan',$program['lama_pendidikan']) ?></div>
+    <div>Persyaratan : <?php echo $this->editor->textarea('persyaratan',$program['persyaratan']) ?></div>
+    <div>Materi : <?php echo $this->editor->textarea('materi',$program['materi']) ?></div>
+    <div>Pelaksana : <?php echo $this->editor->textarea('pelaksana',$program['pelaksana']) ?></div>
+    <div>Fasilitator : <?php echo $this->editor->textarea('fasilitator',$program['fasilitator']) ?></div>
+    <div>Jumlah peserta : <input type="text" name="jumlah_peserta" value="<?php echo $program['jumlah_peserta'] ?>"/></div>
+    <div>Tempat : <?php echo $this->editor->textarea('tempat',$program['tempat']) ?></div>
+    <div>Tahun program : <input type="text" name="tahun_program" value="<?php echo $program['tahun_program'] ?>"/></div>
     <div><input type="submit" value="Simpan"></div>
 </form>
