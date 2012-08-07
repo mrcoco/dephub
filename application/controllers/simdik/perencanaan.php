2139d3f7d2c1b36a5b17865e92a66c3e9260ff41
Bharata Kalbuaji <18209018@std.stei.itb.ac.id>
2012-08-07 14:32:57 +0700

    Commit controller&view perencanaan sama model perencanaan
    Signed-off-by: Bharata Kalbuaji <18209018@std.stei.itb.ac.id>

--- application/controllers/simdik/perencanaan.php
+++ application/controllers/simdik/perencanaan.php
@@ -11,10 +11,18 @@
  * @author irhamnurhalim
  */
 class Perencanaan extends Perencanaan_Controller{
-
-    public function index()
-    {
+    protected $thn_default;
+    public function __construct() {
+        parent::__construct();
+        $this->thn_default = date('Y');
+        $this->load->model('mdl_perencanaan','rnc');
+    }
+    
+    public function index(){
+        //$this->load->library('lib_perencanaan');
 	$data['title']='Bidang Perencanaan';
+        $data['kategori']=$this->rnc->get_kategori();
+        $data['program']=$this->rnc->get_program($this->thn_default);
 	$this->template->display('main/perencanaan/index',$data);
     }
 }
--- application/models/mdl_perencanaan.php
+++ application/models/mdl_perencanaan.php
@@ -13,6 +13,26 @@ class Mdl_perencanaan extends CI_Model{
     /**
      * CRUD Course
      */
+    
+    function get_program($thn){
+        $this->db->select('id,name,parent');
+        $program = $this->db->get_where('program',array('tahun_program'=>$thn));
+        if($program->num_rows()>0){
+            return $program->result_array();
+        }else{
+            return FALSE;
+        }
+    }
+    
+    function get_kategori(){
+        $kategori = $this->db->get('kategori');
+        if($kategori->num_rows()>0){
+            return $kategori->result_array();
+        }else{
+            return FALSE;
+        }
+    }
+    
 }
 
 /* End of file course.php */
