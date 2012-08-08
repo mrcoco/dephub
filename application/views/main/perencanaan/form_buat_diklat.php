98e57cbbc007e982692d76e8abcb22224fa80cdb
Teguh Nugraha <teguh.nugraha91@gmail.com>
2012-08-08 09:08:53 +0700

    tambah desain

--- application/views/main/perencanaan/form_buat_diklat.php
+++ application/views/main/perencanaan/form_buat_diklat.php
@@ -9,58 +9,59 @@
             <li><a href="#pelaksana" data-toggle="tab">Pelaksana dan Fasilitator</a></li>
             <li><a href="#materi" data-toggle="tab">Materi</a></li>
         </ul>
-<div class="tab-content">
-  <div class="tab-pane active" id="overview">
-    <div class="control-group">
-        <label class="control-label" for="input01">Nama</label>
-        <div class="controls">
-            <input type="text" class="input-xlarge" id="input01" name="name" />
+        <div class="tab-content">
+            <div class="tab-pane active" id="overview">
+                <div class="control-group">
+                    <label class="control-label" for="input01">Nama</label>
+                    <div class="controls">
+                        <input type="text" class="input-xlarge" id="input01" name="name" />
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="input02">Tahun program</label>
+                    <div class="controls">
+                        <input type="text" class="input-mini" id="input02" name="tahun_program" />
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="input03">Kategori</label>
+                    <div class="controls">
+                        <?php echo form_dropdown('kategori', $pil_kategori) ?>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="input04">Deskripsi</label>
+                    <div class="controls">
+                        <?php echo $this->editor->textarea('deskripsi') ?>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane" id="tujuan">
+                <div>Tujuan : <?php echo $this->editor->textarea('tujuan') ?></div>
+                <div>Indikator : <?php echo $this->editor->textarea('indikator') ?></div>
+            </div>
+            <div class="tab-pane" id="pelaksanaan">
+                <div>Tanggal mulai : <input type="date" name="tanggal_mulai"/></div>
+                <div>Tanggal selesai : <input type="date" name="tanggal_akhir"/></div>
+                <div>Lama pendidikan : <?php echo $this->editor->textarea('lama_pendidikan') ?></div>
+                <div>Cara Pelaksanaan Kegiatan: <?php echo $this->editor->textarea('pelaksanaan') ?></div>
+                <div>Tempat : <?php echo $this->editor->textarea('tempat') ?></div>
+            </div>
+            <div class="tab-pane" id="peserta">
+                <div>Jumlah peserta : <input type="text" name="jumlah_peserta"/></div>
+                <div>Persyaratan : <?php echo $this->editor->textarea('persyaratan') ?></div>
+            </div>
+            <div class="tab-pane" id="pelaksana">
+                <div>Pelaksana : <?php echo $this->editor->textarea('pelaksana') ?></div>
+                <div>Fasilitator : <?php echo $this->editor->textarea('fasilitator') ?></div>
+            </div>
+            <div class="tab-pane" id="materi">
+                <div>Materi : <?php echo $this->editor->textarea('materi') ?></div>
+            </div>
         </div>
-    </div>
-    <div class="control-group">
-        <label class="control-label" for="input02">Tahun program</label>
-        <div class="controls">
-            <input type="text" class="input-xlarge" id="input02" name="tahun_program" />
+        <div class="form-actions">
+            <button type="submit" class="btn btn-primary">Simpan</button>
+            <button type="reset" class="btn">Ulangi</button>
         </div>
-    </div>
-    <div class="control-group">
-        <label class="control-label" for="input03">Kategori</label>
-        <div class="controls">
-            <?php echo form_dropdown('kategori', $pil_kategori) ?>
-        </div>
-    </div>
-    <div class="control-group">
-        <label class="control-label" for="input04">Deskripsi</label>
-        <div class="controls">
-            <?php echo $this->editor->textarea('deskripsi') ?>
-        </div>
-    </div>
-  </div>
-  <div class="tab-pane" id="tujuan">
-    <div>Tujuan : <?php echo $this->editor->textarea('tujuan') ?></div>
-    <div>Indikator : <?php echo $this->editor->textarea('indikator') ?></div>
-  </div>
-  <div class="tab-pane" id="pelaksanaan">
-    <div>Tanggal mulai : <input type="date" name="tanggal_mulai"/></div>
-    <div>Tanggal selesai : <input type="date" name="tanggal_akhir"/></div>
-    <div>Lama pendidikan : <?php echo $this->editor->textarea('lama_pendidikan') ?></div>
-    <div>Cara Pelaksanaan Kegiatan: <?php echo $this->editor->textarea('pelaksanaan') ?></div>
-    <div>Tempat : <?php echo $this->editor->textarea('tempat') ?></div>
-  </div>
-  <div class="tab-pane" id="peserta">
-    <div>Jumlah peserta : <input type="text" name="jumlah_peserta"/></div>
-    <div>Persyaratan : <?php echo $this->editor->textarea('persyaratan') ?></div>
-  </div>
-  <div class="tab-pane" id="pelaksana">
-    <div>Pelaksana : <?php echo $this->editor->textarea('pelaksana') ?></div>
-    <div>Fasilitator : <?php echo $this->editor->textarea('fasilitator') ?></div>
-  </div>
-  <div class="tab-pane" id="materi">
-    <div>Materi : <?php echo $this->editor->textarea('materi') ?></div>
-  </div>
-</div>
-<div class="form-actions">
-    <button type="submit" class="btn btn-primary">Simpan</button>
-    <button type="reset" class="btn">Ulangi</button>
-</div>
+    </fieldset>
 </form>
--- application/views/main/perencanaan/index.php
+++ application/views/main/perencanaan/index.php
@@ -1,103 +1,108 @@
-Daftar Diklat Tahun 
+<h2>Daftar Diklat Tahun <?php echo date('Y'); ?></h2>
 <div>
-    DIKLAT PRAJABATAN
-	<ol>
-    <?php $this->lib_perencanaan->print_child($program,1) ?>
-	</ol>
-	</div>
+    <h3>DIKLAT PRAJABATAN</h3>
+    <div class="accordion">
+        <?php
+        $kiri = '<div class="accordion-group"><div class="accordion-heading"><h4>';
+        $kanan = '</h4></div></div>';
+        $kelas = 'accordion-toggle';
+        $this->lib_perencanaan->print_child($program, 1, $kiri, $kanan, $kelas)
+        ?>
+    </div>
+</div>
 <div>
-    DIKLAT DALAM JABATAN
-                            <div class="accordion" id="accordion1">
-                                <div class="accordion-group">
-                                    <div class="accordion-heading">
-                                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapse1a">
-                                            Diklat Kepemimpinan
-                                        </a>
-                                    </div>
-                                    <div id="collapse1a" class="accordion-body collapse in">
-                                        <div class="accordion-inner">
-                                            <ol>
-				<?php $this->lib_perencanaan->print_child($program,4) ?>
-                                            </ol>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="accordion-group">
-                                    <div class="accordion-heading">
-                                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapse1b">
-                                            Diklat Fungsional
-                                        </a>
-                                    </div>
-                                    <div id="collapse1b" class="accordion-body collapse in">
+    <h3>DIKLAT DALAM JABATAN</h3>
+    <div class="accordion" id="accordion1">
+        <div class="accordion-group">
+            <div class="accordion-heading">
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapse1a">
+                    <h4>Diklat Kepemimpinan</h4>
+                </a>
+            </div>
+            <div id="collapse1a" class="accordion-body collapse">
+                <div class="accordion-inner">
+                    <ol>
+                        <?php $this->lib_perencanaan->print_child($program, 4, '<li>', '</li>', '') ?>
+                    </ol>
+                </div>
+            </div>
+        </div>
+        <div class="accordion-group">
+            <div class="accordion-heading">
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapse1b">
+                    <h4>Diklat Fungsional</h4>
+                </a>
+            </div>
+            <div id="collapse1b" class="accordion-body collapse">
 
-                                        <div class="accordion-inner">
-                                            <div class="accordion" id="accordion2b">
-                                                <div class="accordion-group">
-                                                    <div class="accordion-heading">
-                                                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2b" href="#collapseOneb">
-                                                            Diklat Fungsional Keahlian
-                                                        </a>
-                                                    </div>
-                                                    <div id="collapseOneb" class="accordion-body collapse">
-                                                        <div class="accordion-inner">
-                                                            <ol>
-				<?php $this->lib_perencanaan->print_child($program,8) ?>
-                                                            </ol>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="accordion-group">
-                                                    <div class="accordion-heading">
-                                                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2b" href="#collapseTwob">
-                                                            Diklat Tehnis Fungsional
-                                                        </a>
-                                                    </div>
-                                                    <div id="collapseTwob" class="accordion-body collapse">
-                                                        <div class="accordion-inner">
-                                                            <ol>
-				<?php $this->lib_perencanaan->print_child($program,9) ?>
-                                                            </ol>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                <div class="accordion-inner">
+                    <div class="accordion" id="accordion2b">
+                        <div class="accordion-group">
+                            <div class="accordion-heading">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2b" href="#collapseOneb">
+                                    Diklat Fungsional Keahlian
+                                </a>
+                            </div>
+                            <div id="collapseOneb" class="accordion-body collapse">
+                                <div class="accordion-inner">
+                                    <ol>
+                                        <?php $this->lib_perencanaan->print_child($program, 8, '<li>', '</li>', '') ?>
+                                    </ol>
                                 </div>
                             </div>
+                        </div>
+                        <div class="accordion-group">
+                            <div class="accordion-heading">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2b" href="#collapseTwob">
+                                    Diklat Tehnis Fungsional
+                                </a>
+                            </div>
+                            <div id="collapseTwob" class="accordion-body collapse">
+                                <div class="accordion-inner">
+                                    <ol>
+                                        <?php $this->lib_perencanaan->print_child($program, 9, '<li>', '</li>', '') ?>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-<div>
-    DIKLAT TEKNIS
-                                            <div class="accordion" id="accordion3">
-                                                <div class="accordion-group">
-                                                    <div class="accordion-heading">
-                                                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion3" href="#collapse3a">
-                                                            Diklat Teknis Umum
-                                                        </a>
-                                                    </div>
-                                                    <div id="collapse3a" class="accordion-body collapse">
-                                                        <div class="accordion-inner">
-                                                            <ol>
-				<?php $this->lib_perencanaan->print_child($program,6) ?>
-                                                            </ol>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="accordion-group">
-                                                    <div class="accordion-heading">
-                                                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion3" href="#collapse3b">
-                                                            Diklat Teknis Manajemen
-                                                        </a>
-                                                    </div>
-                                                    <div id="collapse3b" class="accordion-body collapse">
-                                                        <div class="accordion-inner">
-                                                            <ol>
-				<?php $this->lib_perencanaan->print_child($program,7) ?>
-                                                            </ol>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-</div>
+    <div>
+        <h3>DIKLAT TEKNIS</h3>
+        <div class="accordion" id="accordion3">
+            <div class="accordion-group">
+                <div class="accordion-heading">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion3" href="#collapse3a">
+                        <h4>Diklat Teknis Umum</h4>
+                    </a>
+                </div>
+                <div id="collapse3a" class="accordion-body collapse">
+                    <div class="accordion-inner">
+                        <ol>
+                            <?php $this->lib_perencanaan->print_child($program, 6, '<li>', '</li>', '') ?>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-group">
+                <div class="accordion-heading">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion3" href="#collapse3b">
+                        <h4>Diklat Teknis Manajemen</h4>
+                    </a>
+                </div>
+                <div id="collapse3b" class="accordion-body collapse">
+                    <div class="accordion-inner">
+                        <ol>
+                            <?php $this->lib_perencanaan->print_child($program, 7, '<li>', '</li>', '') ?>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-<a href="perencanaan/dashboard/buat_diklat">Buat Diklat</a>
+    <a href="perencanaan/dashboard/buat_diklat" class="btn btn-primary">Buat Diklat</a>
