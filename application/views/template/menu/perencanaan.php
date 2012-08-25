1bd75b6d83cf69a9e52d8a15223f47ed11368316
Teguh Nugraha <teguh.nugraha91@gmail.com>
2012-08-25 19:39:43 +0700

    fungsi2 di controller dibagi ke bbrp file views penyelenggaraan dan perencanaan pindah ke simdik

--- application/views/template/menu/perencanaan.php
+++ application/views/template/menu/perencanaan.php
@@ -11,13 +11,15 @@
 		<ul class="nav">
 		    <a class="brand" href="#"><img src="assets/img/dephub-icon.png" /></a>
                     <li class="<?php if($this->uri->segment(1)=='perencanaan' && $this->uri->segment(2)=='dashboard' && $this->uri->segment(3)==''){echo 'active';};?>"><a href="perencanaan/dashboard">Dashboard</a></li>
-                    <li class="dropdown <?php if($this->uri->segment(1)=='perencanaan' && $this->uri->segment(2)=='dashboard' && ($this->uri->segment(2)=='daftar_diklat' || $this->uri->segment(2)=='tambah_diklat')){echo 'active';};?>"><a data-toggle="dropdown" class="dropdown-toggle" href="#">Diklat<b class="caret"></b></a>
+                    <li class="dropdown <?php if($this->uri->segment(1)=='perencanaan' && $this->uri->segment(2)=='diklat'){echo 'active';};?>"><a data-toggle="dropdown" class="dropdown-toggle" href="#">Diklat<b class="caret"></b></a>
                         <ul class="dropdown-menu">
-                            <li class="<?php if($this->uri->segment(1)=='perencanaan' && $this->uri->segment(2)=='dashboard' && $this->uri->segment(3)=='daftar_diklat'){echo 'active';};?>"><a href="perencanaan/dashboard/daftar_diklat">Daftar Diklat</a></li>
-                            <li class="<?php if($this->uri->segment(1)=='perencanaan' && $this->uri->segment(2)=='dashboard' && $this->uri->segment(3)=='buat_diklat'){echo 'active';};?>"><a href="perencanaan/dashboard/buat_diklat">Tambah Diklat</a></li>
+                            <li class="<?php if($this->uri->segment(1)=='perencanaan' && $this->uri->segment(2)=='diklat' && $this->uri->segment(3)=='daftar_diklat'){echo 'active';};?>"><a href="perencanaan/dashboard/daftar_diklat">Daftar Diklat</a></li>
+                            <li class="<?php if($this->uri->segment(1)=='perencanaan' && $this->uri->segment(2)=='diklat' && $this->uri->segment(3)=='buat_diklat'){echo 'active';};?>"><a href="perencanaan/dashboard/buat_diklat">Tambah Diklat</a></li>
                         </ul>
                     </li>
-		    <li class="dropdown <?php if($this->uri->segment(2)=='about'){echo 'active';};?>"><a data-toggle="dropdown" class="dropdown-toggle" href="#">Tentang Kita<b class="caret"></b></a>
+		</ul>
+		<ul class="nav pull-right">
+		    <li class="dropdown <?php if($this->uri->segment(2)=='about'){echo 'active';};?>"><a data-toggle="dropdown" class="dropdown-toggle" href="#">Tentang PSDMAP <b class="caret"></b></a>
 			<ul class="dropdown-menu">
 			    <li class="<?php if($this->uri->segment(1)=='about' && $this->uri->segment(2)==''){echo 'active';};?>"><a href="about">Pusbang SDM</a></li>
 			    <li class="<?php if($this->uri->segment(1)=='about' && $this->uri->segment(2)=='visi_misi'){echo 'active';};?>"><a href="about/visi_misi">Visi & Misi</a></li>
@@ -25,9 +27,6 @@
 			    <li class="<?php if($this->uri->segment(1)=='about' && $this->uri->segment(2)=='kontak'){echo 'active';};?>"><a href="about/kontak">Kontak</a></li>
 			</ul>
 		    </li>
-
-		</ul>
-		<ul class="nav pull-right">
 		    <li class="divider-vertical"></li>
 		    <li><a href="login/logout">Logout</a></li>
 		</ul>
