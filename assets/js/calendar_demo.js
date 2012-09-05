365546856963f11b71090921be446380a28af8ac
Bharata Kalbuaji <18209018@std.stei.itb.ac.id>
2012-09-05 19:09:08 +0700

    ewfweffv  dfgfdgfd
    Signed-off-by: Bharata Kalbuaji <18209018@std.stei.itb.ac.id>

--- assets/js/calendar_demo.js
+++ assets/js/calendar_demo.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
+
+
    var $calendar = $('#calendar');
    var id = 10;
 
    $calendar.weekCalendar({
+      displayOddEven:true,
       timeslotsPerHour : 4,
       allowCalEventOverlap : true,
       overlapEventsSeparate: true,
-      firstDayOfWeek : 0,
-      businessHours :{start: 0, end: 24, limitDisplay: true },
+      firstDayOfWeek : 1,
+      businessHours :{start: 8, end: 18, limitDisplay: true },
       daysToShow : 7,
+      switchDisplay: {'1 day': 1, '3 next days': 3, 'work week': 5, 'full week': 7},
+      title: function(daysToShow) {
+			return daysToShow == 1 ? '%date%' : '%start% - %end%';
+      },
       height : function($calendar) {
          return $(window).height() - $("h1").outerHeight() - 1;
       },
@@ -145,7 +152,51 @@ $(document).ready(function() {
       var day = new Date().getDate();
 
       return {
-         events : []
+         events : [
+            {
+               "id":1,
+               "start": new Date(year, month, day, 12),
+               "end": new Date(year, month, day, 13, 30),
+               "title":"Lunch with Mike"
+            },
+            {
+               "id":2,
+               "start": new Date(year, month, day, 14),
+               "end": new Date(year, month, day, 14, 45),
+               "title":"Dev Meeting"
+            },
+            {
+               "id":3,
+               "start": new Date(year, month, day + 1, 17),
+               "end": new Date(year, month, day + 1, 17, 45),
+               "title":"Hair cut"
+            },
+            {
+               "id":4,
+               "start": new Date(year, month, day - 1, 8),
+               "end": new Date(year, month, day - 1, 9, 30),
+               "title":"Team breakfast"
+            },
+            {
+               "id":5,
+               "start": new Date(year, month, day + 1, 14),
+               "end": new Date(year, month, day + 1, 15),
+               "title":"Product showcase"
+            },
+            {
+               "id":6,
+               "start": new Date(year, month, day, 10),
+               "end": new Date(year, month, day, 11),
+               "title":"I'm read-only",
+               readOnly : true
+            },
+            {
+               "id":7,
+               "start": new Date(year, month, day + 2, 17),
+               "end": new Date(year, month, day + 3, 9),
+               "title":"Multiday"
+            }
+         ]
       };
    }
 
@@ -155,6 +206,9 @@ $(document).ready(function() {
     * form for editing based on the calendar event being edited
     */
    function setupStartAndEndTimeFields($startTimeField, $endTimeField, calEvent, timeslotTimes) {
+
+      $startTimeField.empty();
+      $endTimeField.empty();
 
       for (var i = 0; i < timeslotTimes.length; i++) {
          var startTime = timeslotTimes[i].start;
@@ -170,6 +224,9 @@ $(document).ready(function() {
          $startTimeField.append("<option value=\"" + startTime + "\" " + startSelected + ">" + timeslotTimes[i].startFormatted + "</option>");
          $endTimeField.append("<option value=\"" + endTime + "\" " + endSelected + ">" + timeslotTimes[i].endFormatted + "</option>");
 
+         $timestampsOfOptions.start[timeslotTimes[i].startFormatted] = startTime.getTime();
+         $timestampsOfOptions.end[timeslotTimes[i].endFormatted] = endTime.getTime();
+
       }
       $endTimeOptions = $endTimeField.find("option");
       $startTimeField.trigger("change");
@@ -177,14 +234,15 @@ $(document).ready(function() {
 
    var $endTimeField = $("select[name='end']");
    var $endTimeOptions = $endTimeField.find("option");
+   var $timestampsOfOptions = {start:[],end:[]};
 
    //reduces the end time options to be only after the start time options.
    $("select[name='start']").change(function() {
-      var startTime = $(this).find(":selected").val();
+      var startTime = $timestampsOfOptions.start[$(this).find(":selected").text()];
       var currentEndTime = $endTimeField.find("option:selected").val();
       $endTimeField.html(
             $endTimeOptions.filter(function() {
-               return startTime < $(this).val();
+               return startTime < $timestampsOfOptions.end[$(this).text()];
             })
             );
 
