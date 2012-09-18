cd0e193d689f74fffdf7bc2372fa9cedbb3cc1f1
d_frEak <eric.c13@gmail.com>
2012-09-18 14:14:55 +0700

    pegawai

---
+++ application/views/simdik/penyelenggaraan/list_pegawai.php
@@ -0,0 +1,27 @@
+<script>
+    $(document).ready(function() {
+        load(1,'');
+        $('#cari').keyup(function(){
+            load(1,$(this).val());
+        });
+    });
+    function load(page,filter){
+        $('#body_table').empty();
+        $('#body_table').append('<center>Loading... <img src="<?php echo base_url()?>assets/img/spinner.gif"/></center>');
+        if(filter!=''){
+            $.get('<?php echo base_url()?>penyelenggaraan/pegawai/list_pegawai/'+page+'/'+filter, function(result){
+                $('#body_table').html(result);
+            });
+        }else{
+            $.get('<?php echo base_url()?>penyelenggaraan/pegawai/list_pegawai/'+page, function(result){
+                $('#body_table').empty();
+                $('#body_table').html(result);
+            });
+        }
+    }
+    
+    
+</script>
+
+Search: <input type="text" id="cari" placeholder="Masukkan nama/NIP"/>
+<div id="body_table"></div>    
