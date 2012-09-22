8d832da365071ddcd7cf9ce1ff3fcf10e44bec43
petrusfajar <petrusfajar@gmail.com>
2012-09-22 14:48:36 +0700

    nambahin upload_form.php dan upload_success.php

---
+++ application/views/main/elibrary/upload_form.php
@@ -0,0 +1,51 @@
+<div class="row">
+    <div class="span3">
+        <div class="well sidemenu">
+            <ul class="nav nav-list">
+                <li>TEGUH NUGRAHA</li>
+                <li class="divider"></li>
+                <li><a href="#">Edit Profil</a></li>
+                <li><a href="site/dashboard/library/">Logout</a></li>
+            </ul>
+        </div>
+        <div class="well sidemenu">
+            <ul class="nav nav-list">
+                <li class="nav-header">Menu</li>
+                <li ><a href="#">Home</a></li>
+                <li><a href="#">Riwayat</a></li>
+                <li><a href="#">Favorit</a></li>
+				<li class="active"><a href="http://localhost/dephub/site/dashboard/library/upload_form">Upload</a></li>
+                <li class="nav-header">Kategori file</li>
+                <li ><a href="#"><i class="icon-file icon-white"></i>Kategori 1</a></li>
+                <li><a href="#"><i class="icon-film"></i>Kategori 2</a></li>
+                <li><a href="#"><i class="icon-picture"></i>Kategori 3</a></li>
+                <li><a href="#"><i class="icon-book"></i>Kategori 4</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Peraturan</a></li>
+                <li><a href="#">Tentang E-library</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="span9">
+        <form method="GET" action="">
+            <div class="input-append">
+                <input type="text" name="search" placeholder="Cari file" class="input-xlarge"/><button class="btn" type="button">Search</button>
+            </div>
+        </form>   
+
+			
+			<?php echo form_open_multipart('elibrary/upload/do_upload');?>
+			<table>
+				<input type="file" name="userfile" size="20" /> <br />
+				<input type="text" name="" size="20" />
+			</table>
+
+			<br /><br />
+
+			<input type="submit" value="upload" />
+
+			</form>
+        
+        <d
+    </div>
+</div>
