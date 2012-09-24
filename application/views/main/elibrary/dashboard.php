2de60df299b094e0851958726ce2fdbf15db550d
petrusfajar <petrusfajar@gmail.com>
2012-09-24 14:24:02 +0700

    tambahan elibrary dashboard.php

--- application/views/main/elibrary/dashboard.php
+++ application/views/main/elibrary/dashboard.php
@@ -1,31 +1,5 @@
-<div class="row">
-    <div class="span3">
-        <div class="well sidemenu">
-            <ul class="nav nav-list">
-                <li>TEGUH NUGRAHA</li>
-                <li class="divider"></li>
-                <li><a href="#">Edit Profil</a></li>
-                <li><a href="site/dashboard/library/">Logout</a></li>
-            </ul>
-        </div>
-        <div class="well sidemenu">
-            <ul class="nav nav-list">
-                <li class="nav-header">Menu</li>
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Riwayat</a></li>
-                <li><a href="#">Favorit</a></li>
-				<li><a href="http://localhost/dephub/site/dashboard/library/upload_form">Upload</a></li>
-                <li class="nav-header">Kategori file</li>
-                <li ><a href="#"><i class="icon-file icon-white"></i>Kategori 1</a></li>
-                <li><a href="#"><i class="icon-film"></i>Kategori 2</a></li>
-                <li><a href="#"><i class="icon-picture"></i>Kategori 3</a></li>
-                <li><a href="#"><i class="icon-book"></i>Kategori 4</a></li>
-                <li class="divider"></li>
-                <li><a href="#">Peraturan</a></li>
-                <li><a href="#">Tentang E-library</a></li>
-            </ul>
-        </div>
-    </div>
+
+    
     <div class="span9">
         <form method="GET" action="">
             <div class="input-append">
@@ -68,4 +42,3 @@
             </div>
         </div>
     </div>
-</div>
