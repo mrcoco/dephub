cf73c7ed23515d02b87bf7eaeeaff316bb570142
petrusfajar <petrusfajar@gmail.com>
2012-09-25 14:41:08 +0700

    commit dashboard.php untuk library

--- application/controllers/site/dashboard.php
+++ application/controllers/site/dashboard.php
@@ -58,21 +58,10 @@ class Dashboard extends CI_Controller {
 	$this->template->display('main/uc', $data);
     }
 
-    function library($id="public") {
-	$data['title'] = 'Perpustakaan';
-        if($id=="user"){
-            $this->template->display('main/elibrary/user', $data);            
-<<<<<<< HEAD
-        }
-		else{
-=======
-        }else if($id=="upload_form"){
-            $this->template->display('main/elibrary/upload_form', $data);
-	}else{
->>>>>>> b9f510c22de388156e6cde44364246432dbe9b52
+    function library() {
+	
             $this->template->display('main/elibrary/index', $data);
-        }
-//>>>>>>> ad4a2d0ca07040c564ebb355b799fbb9f469d17b
+
     }
 
     /*
