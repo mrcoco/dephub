d5a11fad6b5843eb77d9d41cd24f00703cf3cb75
Bharata Kalbuaji <18209018@std.stei.itb.ac.id>
2012-10-08 12:40:55 +0700

    Mindahin DOMPDF
    Signed-off-by: Bharata Kalbuaji <18209018@std.stei.itb.ac.id>

--- application/helpers/pdfexport_helper.php
+++ application/helpers/pdfexport_helper.php
@@ -13,15 +13,22 @@
 *
 */
 
-function pdf_create($html, $filename, $stream=TRUE) 
-{
-    require_once("dompdf/dompdf_config.inc.php");
-
+function pdf_create($html, $filename, $stream=TRUE) {
+    require_once("/application/third_party/pdf/dompdf_config.inc.php");
+    
     $dompdf = new DOMPDF();
     $dompdf->load_html($html);
-    $dompdf->set_paper("a4", "portrait" );
+    $dompdf->set_paper("A4", "portrait" );
     $dompdf->render();
     $dompdf->stream($filename . ".pdf");
 }
 
-?>
+function pdf_landscape($html, $filename, $stream=TRUE) {
+    require_once("/application/third_party/pdf/dompdf_config.inc.php");
+    
+    $dompdf = new DOMPDF();
+    $dompdf->load_html($html);
+    $dompdf->set_paper("A4", "landscape" );
+    $dompdf->render();
+    $dompdf->stream($filename . ".pdf");
+}
