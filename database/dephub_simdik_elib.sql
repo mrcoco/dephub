0d67538819bc7106a7498b53c090e91ddc46d6a5
petrusfajar <petrusfajar@gmail.com>
2012-10-31 10:31:11 +0700

    update database

--- database/dephub_simdik_elib.sql
+++ database/dephub_simdik_elib.sql
@@ -7,7 +7,7 @@ DROP TABLE  `tb_elib_author` ,
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 17, 2012 at 08:54 AM
+-- Generation Time: Oct 31, 2012 at 04:29 AM
 -- Server version: 5.5.8
 -- PHP Version: 5.3.5
 
@@ -53,7 +53,7 @@ INSERT INTO `tb_elib_author` (`idauthor`, `authorname`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `tb_elib_bibliography` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `id` int(7) NOT NULL AUTO_INCREMENT,
   `title` text NOT NULL,
   `idauthor` int(5) NOT NULL,
   `idcategory` int(2) NOT NULL,
@@ -63,16 +63,17 @@ CREATE TABLE IF NOT EXISTS `tb_elib_bibliography` (
   `keterangan` text NOT NULL,
   `tags` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=42 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=43 ;
 
 --
 -- Dumping data for table `tb_elib_bibliography`
 --
 
 INSERT INTO `tb_elib_bibliography` (`id`, `title`, `idauthor`, `idcategory`, `iduploader`, `type`, `location`, `keterangan`, `tags`) VALUES
-(39, 'Coupling', 3, 4, 0, 1, './assets/elibrary/uploads/Coupling.pdf', '      ini adalah keterangan      ', '    keyword 1 keyword 2, keyword 3      '),
+(39, 'Coupling', 1, 4, 0, 1, './assets/elibrary/uploads/Coupling.pdf', 'test  ', '    keyword 1 keyword 2, keyword 3         '),
 (40, 'Psiko_tes', 1, 1, 0, 4, './assets/elibrary/uploads/Psiko_tes.docx', 'keterangan psiko tes', ' psiko tes'),
-(41, 'merkblatt_zulass_eng', 1, 1, 0, 1, './assets/elibrary/uploads/merkblatt_zulass_eng.pdf', 'bahasa jerman', 'jerman ');
+(41, 'merkblatt_zulass_eng', 1, 1, 0, 1, './assets/elibrary/uploads/merkblatt_zulass_eng.pdf', 'bahasa jerman', 'jerman '),
+(42, 'Petrus_S2_-_ITB', 1, 1, 0, 1, './assets/elibrary/uploads/Petrus_S2_-_ITB.pdf', 'dsfdf', ' dfsf');
 
 -- --------------------------------------------------------
 
@@ -81,11 +82,11 @@ INSERT INTO `tb_elib_bibliography` (`id`, `title`, `idauthor`, `idcategory`, `id
 --
 
 CREATE TABLE IF NOT EXISTS `tb_elib_books` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `id` int(7) NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
   `idauthor` int(5) NOT NULL,
   `edition` int(4) NOT NULL,
-  `category` int(3) NOT NULL,
+  `idcategory` int(3) NOT NULL,
   `frequency` int(2) NOT NULL,
   `issnisbn` int(15) NOT NULL,
   `publisher` varchar(50) NOT NULL,
@@ -96,12 +97,18 @@ CREATE TABLE IF NOT EXISTS `tb_elib_books` (
   `keterangan` text NOT NULL,
   `tags` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `tb_elib_books`
 --
 
+INSERT INTO `tb_elib_books` (`id`, `title`, `idauthor`, `edition`, `idcategory`, `frequency`, `issnisbn`, `publisher`, `publisherplace`, `publisheryear`, `stock`, `digital`, `keterangan`, `tags`) VALUES
+(1, 'coupling', 3, 1, 4, 0, 2147483647, 'Galamedia', 'Jakarta', 1995, 3, 40, 'Buku ini ada digitalnya', 'fisika'),
+(2, 'Buku dummy majalah', 1, 1, 39, 1, 1241241245, 'Galamedia', 'Jakarta', 1993, 3, 0, 'keterangan 1 ada ', ''),
+(3, 'buku 3', 1, 1, 1, 2, 3232, 'Gramedia', 'Bandung', 0, 2, 0, 'qwe', ' wqe'),
+(4, 'buku 4', 1, 1, 1, 2, 3232, 'Gramedia', 'Bandung', 0, 2, 0, 'qwe', ' wqe'),
+(5, 'buku 5', 1, 1, 1, 2, 3232, 'Gramedia', 'Bandung', 0, 2, 0, 'qwe', ' wqe');
 
 -- --------------------------------------------------------
 
