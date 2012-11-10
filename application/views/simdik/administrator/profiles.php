0dccf48df0334376d8bcbfcfc6d9c832bfdc603d
Bharata <18209018@std.stei.itb.ac.id>
2012-11-10 15:29:21 +0700

    Ganti library, model, helper, sama views
    Signed-off-by: Bharata <18209018@std.stei.itb.ac.id>

--- application/views/simdik/administrator/profiles.php
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript" src="assets/js/bootstrap-modal.js"></script>
-<p>
-    <?php echo $table_user;?>
-</p>
-<?php echo $modal_user;?>
--- application/views/template/header.php
+++
@@ -1,5 +0,0 @@
-<div class="row">
-    <div class="span-24">
-        <div id="header"></div>
-    </div>
-</div>
