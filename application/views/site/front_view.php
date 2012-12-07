eeafced23fa03d220bd43f822681f9a9d6bac5fb
Teguh Nugraha <teguh.nugraha91@gmail.com>
2012-12-07 08:14:00 +0700

    daftar diklat di depan

--- application/views/site/front_view.php
+++ application/views/site/front_view.php
@@ -1,11 +1,9 @@
-<script type="text/javascript" src="assets/js/bootstrap-tab.js"></script>
-<script type="text/javascript" src="assets/js/bootstrap-modal.js"></script>
 <script type='text/javascript' src='assets/js/fullcalendar/fullcalendar.min.js'></script>
 <script type="text/javascript" language="javascript" src="assets/js/swfobject.js"></script>
 <div class="row"><div class="span9"><?php echo $this->session->flashdata('msg'); ?></div></div>
 <ul class="nav nav-tabs">
     <li class="active"><a href="#pengumuman" data-toggle="tab">Pengumuman</a></li>
-    <li><a href="#kalender" id="cal" data-toggle="tab">Kalender Diklat</a></li>
+    <li><a href="#diklat" data-toggle="tab">Daftar Diklat</a></li>
 </ul>
 
 <div class="tab-content">
@@ -17,9 +15,18 @@
         <hr />
         <?php } ?>
     </div>
-    <div class="tab-pane" id="kalender">
-        <div id='loading' style='display:none'>loading...</div>
-        <div id='calendar' style="width:690px;"></div>
+    <div class="tab-pane" id="diklat">
+        <?php if($program){ ?>
+                <table width="100%" class="table-striped table-condensed table">
+                    <?php $this->lib_perencanaan->print_tree_table_pub($program)?>
+                </table>
+        <?php }else{?>
+        Tidak ada data
+        <?php } ?>
+        <div class="form-actions">
+            <a class="btn btn-primary" href="<?php echo base_url()?>site/kal_diklat">Lihat Kalender</a>
+            <a class="btn btn-success" href="<?php echo base_url()?>diklat/cetak_jadwal">Cetak Jadwal</a>
+        </div>
     </div>
 </div>
 <script type="text/javascript">
