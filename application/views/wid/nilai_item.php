d742d17dcdb75a91969ba8dfa27ffdf963e280a0
Teguh Nugraha <teguh.nugraha91@gmail.com>
2013-03-21 12:02:31 +0700

    update nilai_item.php

--- application/views/wid/nilai_item.php
+++ application/views/wid/nilai_item.php
@@ -19,16 +19,14 @@ $(function(){
 <form action="wid/nilai/item_insert" method="POST">
 <input type="hidden" name="id_materi" value="<?php echo $indeks['id_materi']?>"/>
 <input type="hidden" name="id_program" value="<?php echo $indeks['id_program']?>"/>
-<?php foreach($list_komponen as $l){?>
 <div class="cont">
+<?php foreach($list_komponen as $l){?>
     <p class="input-append input-prepend">
         <input type="text" class="item" name="item[]" placeholder="Unsur Penilaian" value="<?php echo $l['nama_komponen']?>"/>
         <input type="text" class="bobot input-small" name="bobot[]" placeholder="Bobot" value="<?php echo $l['bobot']?>"/><span class="add-on">%</span>
         <span class="btn btn-mini btn-danger del"><i class="icon-remove"></i> Hapus</span>
     </p>
-</div>
 <?php } ?>
-<div class="cont">
     <p class="input-append input-prepend">
         <input type="text" class="item" name="item[]" placeholder="Unsur Penilaian"/>
         <input type="text" class="bobot input-small" name="bobot[]" placeholder="Bobot"/><span class="add-on">%</span>
